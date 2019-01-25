38deb71cba44b4ad38c0a47aacbe7e49ff2543cf
Merkhad Luigton <Inouchi@outlook.com>
2019-01-25 09:22:57 -0300

    Update ibus.py

--- ibus.py
+++ ibus.py
@@ -124,7 +124,7 @@ class UniEmojiIBusEngine(IBus.Engine):
                 self.invalidate()
                 return True
             elif keyval in num_keys:
-                index = num_keys.index(keyval) 
+                index = num_keys.index(keyval)
                 if self.set_lookup_table_cursor_pos_in_current_page(index):
                     self.commit_candidate()
                     return True
